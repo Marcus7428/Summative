0423c72a3744e467d5003ce1841c0cee67d90475
Marcus Guo <marcus.guo@student.tdsb.on.ca>
2025-06-06 13:54:55 -0400

    Need to fix code, website not loading

--- Summative/src/App.jsx
+++ Summative/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeView from "./views/HomeView";
+import MoviesView from "./views/MoviesView";
+import MovieDetailView from "./views/MovieDetailView";
+import GenreView from "./views/GenreView";
+import SearchView from "./views/SearchView"; // Import SearchView
+import LoginView from "./views/LoginView";
+import RegisterView from "./views/RegisterView";
+import ErrorView from "./views/ErrorView";
+import CartView from "./views/CartView";
+import SettingView from "./views/SettingView";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomeView />} />
+                <Route path="/movies" element={<MoviesView />}>
+                    <Route path="details/:id" element={<MovieDetailView />} />
+                    <Route path=":genre_id" element={<GenreView />} />
+                    <Route path="search" element={<SearchView />} />
+                </Route>
+                <Route path="/cart" element={<CartView />} />
+                <Route path="/settings" element={<SettingView />} />
+                <Route path="/login" element={<LoginView />} />
+                <Route path="/register" element={<RegisterView />} />
+                <Route path="*" element={<ErrorView />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
